515ad809d6235586424fd000e00b00184795c3d3
Cem <cicekcem25@gmail.com>
2021-01-20 16:08:28 +0300

    Initial commit

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri ekle)
+        {
+            Console.WriteLine("Müşteri Eklendi: " + ekle.Adi+ " " + ekle.Soyadi);
+
+        }
+        public void MusteriSil(Musteri sil)
+        {
+            Console.WriteLine("Müşteri Silindi: " + sil.Adi+ " " + sil.Soyadi);
+        }
+        public void MusteriListele(Musteri[] liste)
+        {
+            Console.WriteLine("Müşteri listesi...");
+            foreach (Musteri  mtr in liste)
+            {
+                Console.WriteLine("Id: " + mtr.id);
+                Console.WriteLine("Adi: " + mtr.Adi);
+                Console.WriteLine("Soyadi: " + mtr.Soyadi);
+                Console.WriteLine("Tel: " + mtr.MusteriTel);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.id = 1;
+            musteri1.Adi = "Cem";
+            musteri1.Soyadi = "Çiçek";
+            musteri1.MusteriTel = 55555555;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.id = 1;
+            musteri2.Adi = "Emir";
+            musteri2.Soyadi = "Çiçek";
+            musteri2.MusteriTel = 55555555;
+
+            MusteriManager musteriManager = new MusteriManager();
+            Console.WriteLine("--Müşteri Ekleme--");
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+            Console.WriteLine("---------------------------------");
+
+            Console.WriteLine("--Müşteri Silme--");
+            musteriManager.MusteriSil(musteri1);
+            musteriManager.MusteriSil(musteri2);
+            Console.WriteLine("---------------------------------");
+
+            Console.WriteLine("--Müşteri Liteleme Metodu--");
+            Musteri[] musteriliste = new Musteri[] { musteri1, musteri2 };
+            musteriManager.MusteriListele(musteriliste);
+
+            Console.ReadLine();
+        }
+    }
+}
